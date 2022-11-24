f3ef8bc62e5136e3fbbf7b07a64993522ef8972f
AlexandreLeonetti <al.leo@wanadoo.fr>
2022-11-24 16:32:24 +0100

    getSingleLevel

--- components/calculate/ContainsFlashClass.js
+++ components/calculate/ContainsFlashClass.js
@@ -284,7 +284,7 @@ async sendLevel () {
                     </Link>
                                         <button
 												className=" cursor-pointer mt-5 bg-white text-black font-black w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base" 
-                                                onClick={() => this.getLevel()}
+                                                onClick={() => this.getSingleLevel()}
 																		>Current Level
 										</button>
 				 </>
