4910dad676aa6fa4ed76412c38fc925e06926d1c
AlexandreLeonetti <al.leo@wanadoo.fr>
2022-11-26 19:11:15 +0100

    handleFlashing

--- components/calculate/ContainsFlashClass.js
+++ components/calculate/ContainsFlashClass.js
@@ -207,7 +207,7 @@ class ContainsFlashClass extends Component{
                </div>
                <div className="grid grid-cols-1 ">
 				 {mode=="home" && <>
-				    <div	className=" inline-flex items-center justify-center">
+                     {/*<div	className=" inline-flex items-center justify-center">
 				        <div className="py-20 inline-flex items-center justify-center">
   	  		                <button 
 				    	        className="disk bg-emerald-400 text-4xl text-white w-40 font-black inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-2xl shadow-sm text-base" 
@@ -218,8 +218,11 @@ class ContainsFlashClass extends Component{
 						     <div className="circle bg-emerald-500 delay2"></div>
 						     <div className="circle bg-emerald-500 delay3"></div>
 				        </div>
-                    </div>
-                     <LoadLevel greetHandler={this.greetParent}/>
+                    </div> */}
+                     <LoadLevel 
+                            greetHandler={this.greetParent}
+                            flashHandler={this.handleStartFlashing()}
+                     />
                     <Link href="/">
                                         <div
 												className=" cursor-pointer mt-5 bg-white text-black font-black w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base" 
